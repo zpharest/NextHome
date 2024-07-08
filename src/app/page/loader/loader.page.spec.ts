d923c39128c620bde1f252d1274ec2fb7cc23dfb
zpharest <viniciusmpires2007@gmail.com>
2024-07-08 20:05:47 -0300

    retirada de templates inuteis

---
+++ src/app/page/loader/loader.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoaderPage } from './loader.page';
+
+describe('LoaderPage', () => {
+  let component: LoaderPage;
+  let fixture: ComponentFixture<LoaderPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoaderPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
