e5c325661d08cc3d0d276e35f93fe9b0b335f30e
zpharest <viniciusmpires2007@gmail.com>
2024-09-30 21:57:31 -0300

    Merge branch 'main' of https://github.com/zpharest/NextHome

--- src/app/page/loader/loader.page.ts
+++ src/app/page/loader/loader.page.ts
@@ -11,6 +11,8 @@ export class LoaderPage implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-  }
-
+    setTimeout(()=>{
+    this.router.navigate(['login']);
+  },1000)
+}
 }
